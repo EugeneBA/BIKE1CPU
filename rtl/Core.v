266766850505a423d446997769d00180e626f033
Eugene <eugene.bars@outlook.com>
2024-11-14 20:29:46 +0300

    Fix compile errors

--- rtl/Core.v
+++ rtl/Core.v
@@ -1,9 +1,16 @@
-module core (
+module Core 
+(
     input wire clk,                // Тактовый сигнал
     input wire reset,              // Сигнал сброса     
-	 output reg [6:0] HEX1
-    output reg [6:0] HEX0
-    output reg [6:0] HEX2	 // Светодиоды для отображения результата
+	 
+	 output reg [7:0] outLedG,
+	 output reg [9:0] outLedR,
+	 
+	 output reg [6:0] out7Seg0,
+    output reg [6:0] out7Seg1,
+    output reg [6:0] out7Seg2,
+	 output reg [6:0] out7Seg3
+	 
 );
 
     reg [31:0] command_reg;         // Командный регистр
@@ -16,7 +23,9 @@ module core (
         command_memory[1] = 4'b0010; // Пример команды
         // Остальные команды можно заполнить по необходимости
     end
-    SEG7_LUT shema_vs (.oSEG(HEX0[6:0]) ,.iDIG(pc[3:0]) )
+    SEG7_LUT shema_vs (.oSEG(out7Seg0[6:0]) ,.iDIG(pc[3:0]));
+	 
+	 
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             pc <= 0;                // Сброс счётчика команд
@@ -26,10 +35,10 @@ module core (
             pc <= pc + 1;           // Увеличиваем счётчик команд
 
             case (command_reg)
-                4'b0001: leds <= 4'b0001; // Пример команды: зажечь светодиод 0
-                4'b0010: leds <= 4'b0010; // Пример команды: зажечь светодиод 1
+                4'b0001: outLedG <= 4'b0001; // Пример команды: зажечь светодиод 0
+                4'b0010: outLedG <= 4'b0010; // Пример команды: зажечь светодиод 1
                 // Добавьте другие команды здесь
-                default: leds <= 4'b0000; // NOP: ничего не делать
+                default: outLedG <= 4'b0000; // NOP: ничего не делать
             endcase
         end
     end
